d08c8bb13ffdecdb749b43fd0d18751b7927d801
Riginana <rigina.rysbekova@gmail.com>
2021-04-28 10:20:48 +0200

    eine Veränderung

--- spa_components_conditional-rendering/src/index.js
+++ spa_components_conditional-rendering/src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from "react-dom";
 
 const Alert = () => {
   const colorClasses = {
-    color: "red",
+    color: "orange",
 	  text4Red:"OMG! Something really bad has happended!",
 	  text4Orange:"Well, it is not that bad after all!"
   };
   //your component here
   return(
     <>
-    <div className="alert alert-danger" role="alert">
-        { colorClasses.text4Red }
-
-    </div>
-
-    <div className="alert alert-warning" role="alert">
+    { 
+      (colorClasses.color === "red") ?
+      <div className="alert alert-danger" role="alert">
+      { colorClasses.text4Red }
+      </div> :
+      <div className="alert alert-warning" role="alert">
         { colorClasses.text4Orange }
-    </div>
+      </div>
+    }
     </>
   )
 };
